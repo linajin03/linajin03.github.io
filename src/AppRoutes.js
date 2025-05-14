97e9c6004d2da4c398c4d593248b0c835b9be513
linajin03 <53534844+linajin03@users.noreply.github.com>
2025-05-14 12:22:28 -0400

    replace old website with new:

--- src/AppRoutes.js
+++
@@ -1,9 +0,0 @@
-import Home from './Pages/Home.js';
-import Projects from './Pages/Projects.js';
-
-const AppRoutes = [
-  { path: '/', component: <Home />, exact: true },
-  { path: '/projects', component: <Projects /> }
-];
-
-export default AppRoutes;
