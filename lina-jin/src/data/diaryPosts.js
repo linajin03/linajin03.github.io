952b0e917c465202615a5bd4426c3aacf0216f03
linajin03 <53534844+linajin03@users.noreply.github.com>
2025-05-21 22:09:15 -0400

    adding diary entry

---
+++ lina-jin/src/data/diaryPosts.js
@@ -0,0 +1,22 @@
+import thumbnailCurrentlyMay2025 from '../assets/diary/currently-may-2025/background.jpg';
+import thumbnailMay20 from '../assets/diary/may-20/banner.jpg';
+
+export const diaryPosts = [
+    {
+        title: "currently.. (may 2025)",
+        date: "2025-05-20", 
+        subtitle: "my current favourites of May 2025",
+        tags: ["currently"],
+        url: "/diaryposts/2025/currently-may-2025",
+        thumbnail: thumbnailCurrentlyMay2025,
+    },
+    {
+        title: "spring has sprung in toronto and i captured it through my Olympus XA2",
+        date: "2025-04-20", 
+        subtitle: "sharing some pictures i took around Toronto this spring",
+        tags: ["diary", "pictures"],
+        url: "/diaryposts/currently-april-2025",
+        thumbnail: thumbnailMay20,
+    },
+
+];
