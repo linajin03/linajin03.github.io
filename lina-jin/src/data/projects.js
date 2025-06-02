9243ddd58baf60875d8a478ef26204eb4b011aab
linajin03 <53534844+linajin03@users.noreply.github.com>
2025-06-01 23:56:34 -0400

    adding about me section

--- lina-jin/src/data/projects.js
+++ lina-jin/src/data/projects.js
@@ -2,57 +2,58 @@ export const fileStructure = [
     {
       name: "my-projects",
       type: "folder",
+      img: "/assets/folder.png",
       description: "A collection of my projects. Click on the folders to explore.",
       children: [
         {
-          name: "CS",
+          name: "Track-the-crowd",
           type: "folder",
-          description: "Computer Science projects and resources.",
+          img: "/assets/folder.png",
+          description: "My senior capstone project (CSC490, Interactive Experiences for Social Good). Collaborated with Toronto Transit Commision (TTC)’s New Technology & Innovation team to integrate a low-cost, high efficiency computer based system to reduce crowds during subway boarding times	",
           children: [
+            {
+              name: "arduino.png",
+              type: "tech",
+              img: "/assets/arduino.png",
+              description: "Logo image file.",
+              content: "base64 or URL path",
+            },
             {
               name: "project1.txt",
               type: "file",
-              description: "Details about project 1.",
+              img: "/assets/document.png",
+              description: "Blog post about project 1.",
               content: "Details about project 1",
-            },
-            {
-              name: "Images",
-              type: "folder",
-              description: "Folder containing images related to CS projects.",
-              children: [
-                {
-                  name: "logo.png",
-                  type: "file",
-                  description: "Logo image file.",
-                  content: "base64 or URL path",
-                },
-              ],
             },
           ],
         },
         {
-          name: "STATS",
+          name: "kor-feminism",
           type: "folder",
-          description: "Statistics projects and resources.",
+          img: "/assets/folder.png",
+          description: "My senior research capstone report (AMS312, American Studies: Asian-American Tech Dynamics). In this study, I attempted to analyze the root of the public discomfort around feminism and how this has impacted the public expression of feminism on Korean online platforms, specifically YouTube. I conducted a document research analysis and a sentiment-opinion analysis on comments of specific Korean YouTube videos that discuss Korean feminism.",
           children: [
             {
-              name: "project2.txt",
+              name: "logo.png",
               type: "file",
-              description: "Details about project 2.",
-              content: "Details about project 1",
+              img: "/assets/document.png",
+              description: "Logo image file.",
+              content: "base64 or URL path",
             },
+          ],
+        },
+        {
+          name: "food-prediction",
+          type: "folder",
+          img: "/assets/folder.png",
+          description: "My senior research capstone report (AMS312, American Studies: Asian-American Tech Dynamics). In this study, I attempted to analyze the root of the public discomfort around feminism and how this has impacted the public expression of feminism on Korean online platforms, specifically YouTube. I conducted a document research analysis and a sentiment-opinion analysis on comments of specific Korean YouTube videos that discuss Korean feminism.",
+          children: [
             {
-              name: "Images",
-              type: "folder",
-              description: "Folder containing images related to Statistics projects.",
-              children: [
-                {
-                  name: "logo.png",
-                  type: "file",
-                  description: "Logo image file.",
-                  content: "base64 or URL path",
-                },
-              ],
+              name: "logo.png",
+              type: "file",
+              img: "/assets/document.png",
+              description: "Logo image file.",
+              content: "base64 or URL path",
             },
           ],
         },
