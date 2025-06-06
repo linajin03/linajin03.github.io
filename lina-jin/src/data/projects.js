dfa67a7c83dffcfe34e317dc5256c6e84ffa422e
linajin03 <53534844+linajin03@users.noreply.github.com>
2025-06-05 23:09:47 -0400

    adding more edits

--- lina-jin/src/data/projects.js
+++ lina-jin/src/data/projects.js
@@ -47,7 +47,7 @@ export const fileStructure = [
               img: "/assets/document.png",
               description: "Full report on Korean feminism.",
               content: "/projects/ams312",
-              url: "/pages/projects/ams312"
+              url: "/projects/ams312"
             },
           ],
         },
@@ -55,17 +55,82 @@ export const fileStructure = [
           name: "food-prediction",
           type: "folder",
           img: "/assets/folder.png",
-          description: "My senior research capstone report (AMS312, American Studies: Asian-American Tech Dynamics). In this study, I attempted to analyze the root of the public discomfort around feminism and how this has impacted the public expression of feminism on Korean online platforms, specifically YouTube. I conducted a document research analysis and a sentiment-opinion analysis on comments of specific Korean YouTube videos that discuss Korean feminism.",
+          description: "Developed a machine learning model from scratch leveraging decision trees, regression, Naïve Bayes, and neural networks to predict food items based on 2,000+ survey responses, achieving a 80.18\% prediction accuracy",
           children: [
             {
-              name: "logo.png",
+              name: "report.pdf",
               type: "file",
               img: "/assets/document.png",
               description: "Logo image file.",
               content: "base64 or URL path",
+              url: "/projects/csc311",
+            },
+          ],
+        },
+        {
+          name: "trading-viz",
+          type: "folder",
+          img: "/assets/folder.png",
+          description: "Designed and developed an interactive web application to simplify financial data analysis and strategy performance evaluation for traders without programming expertise",
+          children: [
+            {
+              name: "report.pdf",
+              type: "file",
+              img: "/assets/document.png",
+              description: "Logo image file.",
+              content: "base64 or URL path",
+              url: "/projects/csc311",
+            },
+          ],
+        },
+        {
+          name: "trading-viz",
+          type: "folder",
+          img: "/assets/folder.png",
+          description: "Designed and developed an interactive web application to simplify financial data analysis and strategy performance evaluation for traders without programming expertise",
+          children: [
+            {
+              name: "report.pdf",
+              type: "file",
+              img: "/assets/document.png",
+              description: "Logo image file.",
+              content: "base64 or URL path",
+              url: "/projects/csc311",
             },
           ],
         },
       ],
     },
+    {
+      name: "my-experiences",
+      type: "folder",
+      img: "/assets/folder.png",
+      description: "A collection of my experiences. Double-click on the folder to explore.",
+      children: [
+        {
+          name: "RBC",
+          type: "folder",
+          img: "/assets/rbc.png",
+          date: "",
+          description: "In the summer of 2024, I worked as a developer in RBC Amplify.",
+          children: []
+        },
+        {
+          name: "MCCSS",
+          type: "folder",
+          img: "/assets/folder.png",
+          date: "may-dec 2023",
+          description: "",
+          children: []
+        },
+        {
+          name: "SDSS",
+          type: "folder",
+          img: "/assets/folder.png",
+          date: "aug-may 2025",
+          description: "I co-founded and co-started a club called \"Students in Data Science and Statistics\" to harbour a community of students interested in data science-related fields from all disciplines.",
+          children: []
+        },
+      ]
+    }
 ];
