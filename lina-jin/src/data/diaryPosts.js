3a6031e8121652cddb07f6290dc96a3906815666
linajin03 <53534844+linajin03@users.noreply.github.com>
2025-08-10 21:41:52 -0400

    adding diary aug 10

--- lina-jin/src/data/diaryPosts.js
+++ lina-jin/src/data/diaryPosts.js
@@ -1,5 +1,7 @@
 import thumbnailCurrentlyMay2025 from '../assets/diary/currently-may-2025/background.jpg';
 import thumbnailMay20 from '../assets/diary/may-20/banner.jpg';
+import thumbnailAug10 from "../assets/diary/square/image1.jpg";
+
 
 export const diaryPosts = [
     {
@@ -17,6 +19,14 @@ export const diaryPosts = [
         tags: ["diary", "pictures"],
         url: "/diaryposts/2025/spring-has-sprung",
         thumbnail: thumbnailMay20,
+    },
+    {
+        title: "hard work is a square",
+        date: "2025-08-10", 
+        subtitle: "i graduated from uoft in june!",
+        tags: ["diary"],
+        url: "/diaryposts/2025/hard-work-is-a-square",
+        thumbnail: thumbnailAug10,
     },
 
 ];
